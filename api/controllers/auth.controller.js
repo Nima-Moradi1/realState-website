d793e5a3dbadb51d7543df8c50b3331681bd4cbe
nima moradirad <mnima8100@gmail.com>
2024-01-02 19:08:02 +0330

    finished backend development

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,17 @@
 /* eslint-disable */
 /* prettier-ignore */
-import User from "../models/user.model.js";
-import bcrypt from "bcryptjs";
-import { errorHandler } from "../utils/error.js";
-import jwt from "jsonwebtoken";
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json("User Created Successfully !!");
+    res.status(201).json('User created successfully!');
   } catch (error) {
     next(error);
   }
@@ -21,23 +21,20 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, "User Not Found !!"));
-    const validPassword = bcrypt.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, "Wrong Credentials !!"));
+    if (!validUser) return next(errorHandler(404, 'User not found!'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
     res
-      .cookie("access_token", token, {
-        httpOnly: true,
-        expires: new Date(Date.now() + 24 * 60 * 60 * 7),
-      })
+      .cookie('access_token', token, { httpOnly: true })
       .status(200)
       .json(rest);
   } catch (error) {
     next(error);
   }
 };
-// here i want to authenticate with google
+
 export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
@@ -45,32 +42,39 @@ export const google = async (req, res, next) => {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
       res
-      .cookie("access_token", token, { httpOnly: true })
-      .status(200)
-      .json(rest);
+        .cookie('access_token', token, { httpOnly: true })
+        .status(200)
+        .json(rest);
     } else {
-      // if user doesn't exist , then we're creating one !
-      // since password is required , we should create a random password
-      // so the toString(36) means nums from 0-9 and letters A-Z
-      // and then we slice the last 8 digits of the password
-      const generatedPassword = Math.random().toString(36).slice(-8);
-      const hashedPassword = bcrypt.hashSync(generatedPassword, 10);
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
       const newUser = new User({
-        username:req.body.name.split(" ").join("").toLowerCase() +
-          Math.random().toString(36).slice(-3),
+        username:req.body.name.split(' ').join('').toLowerCase() +
+          Math.random().toString(36).slice(-4),
         email: req.body.email,
         password: hashedPassword,
         avatar: req.body.photo,
       });
-      await newUser.save(); 
-      const token = jwt.sign({id : newUser._id }, process.env.JWT_SECRET)
-      const {password : pass , ...rest } = newUser._doc ;
-      res.cookie('access_token' , token , {httpOnly : true})
-      .status(200)
-      .json(rest) ;
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = newUser._doc;
+      res
+        .cookie('access_token', token, { httpOnly: true })
+        .status(200)
+        .json(rest);
     }
   } catch (error) {
-    // we pass the next error that we created before
+    next(error);
+  }
+};
+
+export const signOut = async (req, res, next) => {
+  try {
+    res.clearCookie('access_token');
+    res.status(200).json('User has been logged out!');
+  } catch (error) {
     next(error);
   }
 };
